83225be18db45a6b9b8a46f274e7346b882751b4
Maksim <40239712+mpozdeev@users.noreply.github.com>
2026-02-26 17:12:02 +0400

    add comments to main
    jenkins -> github -> webhook Testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,7 +23,7 @@ public class Main {
         String csvFileName = "NetflixOriginals.csv";
         MyCsvReader reader = new MyCsvReader(csvFileName);
 
-//      Jenkins -> github -> webhook Testing6
+//      Jenkins -> github -> webhook Testing7
 
 
         Calculus<Film, Map<String, Long>> countLanguages = new CountLanguages();
